b5a4832bc7885c2bd21a1cf26d47f288e795fade
Ryssek <125525568+Ryssek@users.noreply.github.com>
2023-12-10 20:14:54 -0800

    comment change

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
+// Scroll to top
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
